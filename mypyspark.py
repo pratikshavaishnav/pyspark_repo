912fe06b9b2db3fe396e7382a7f6da5b030489bc
pratikshavaishnav <pratiksha.vaishnav.888@gmail.com>
2023-09-17 16:10:15 +0530

    file modified

--- mypyspark.py
+++ mypyspark.py
@@ -7,10 +7,6 @@ spark = SparkSession.builder.master("local[*]").appName("test").getOrCreate()
 inputrawpath="s3://inprawbucket/inputdata/world_bank.json"
 df=spark.read.format("json").option("mode","DROPMALFORMED").load(inputrawpath)
 
-res=df.withColumn("theme1_Name",col("theme1.Name")).withColumn("theme1_Percent",col("theme1.Percent")).drop("theme1")\
-      .withColumn("theme_namecode",explode(col("theme_namecode")))\
-      .withColumn("theme_namecode_code",col("theme_namecode.code"))\
-      .withColumn("theme_namecode_name",col("theme_namecode.name")).drop("theme_namecode")\
 
 
 final=res.select("theme1_Name","theme1_Percent","theme_namecode_code","theme_namecode_name")
