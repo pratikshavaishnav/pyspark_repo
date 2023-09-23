1f16d3b1a5388a83e58f58f04ffc33800fe2ff03
pratikshavaishnav <pratiksha.vaishnav.888@gmail.com>
2023-09-23 17:50:57 +0530

    updated all files

---
+++ python/config.py
@@ -0,0 +1,1 @@
+API_KEY="a07b65bc11ac7fce811218688d68f39d"
---
+++ python/tmdb.py
@@ -0,0 +1,11 @@
+from config import API_KEY
+import requests
+
+def get_movie_data(i):
+	response=requests.get('https://api.themoviedb.org/3/movie/top_rated?api_key={}&&language=en-US&page={}'.format(API_KEY,i))
+	jsondata=response.json()['results']
+	return jsondata
+
+def lambda_handler(event,context)
+    res=get_movie_data(1)
+    print(res)
